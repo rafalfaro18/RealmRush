3a76d384aafaf03c6b24b69209c2c764e274657a
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-27 20:32:00 -0600

    Displays coordinates in text label

---
+++ Assets/CoordinateLabeler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways] // Run In Editor mode.
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake() {
+        label = GetComponent<TextMeshPro>();
+    }
+    
+    void Update()
+    {
+        if(!Application.isPlaying){
+            DisplayCoordinates();
+        }
+    }
+
+    void DisplayCoordinates(){
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.y);
+        label.text = $"{coordinates.x},{coordinates.y}";
+    }
+}
