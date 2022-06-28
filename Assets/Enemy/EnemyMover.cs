d177b4023cee9091bbb3f3c1796a7eccfcc5b2a1
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-27 21:28:58 -0600

    debug

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -9,7 +9,9 @@ public class EnemyMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Start here");
         StartCoroutine(FollowPath());
+        Debug.Log("Finishing start");
     }
 
     IEnumerator FollowPath(){
