b0a5a304d2c74e61afb3d2e7ed345ab8dc631bac
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-28 11:33:01 -0600

    Tower looks at type EnemyMover

---
+++ Assets/Tower/targetLocator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetLocator : MonoBehaviour
+{
+    [SerializeField] Transform weapon;
+    [SerializeField] Transform target;
+    
+    void Start()
+    {
+        target = FindObjectOfType<EnemyMover>().transform;
+    }
+
+    void Update()
+    {
+        AimWeapon();
+    }
+
+    void AimWeapon(){
+        weapon.LookAt(target);
+    }
+}
