5df5b1d9f788633b1e77c80fd1672f4713b9a53e
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-28 00:24:35 -0600

    Smooth enemy movement

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -17,8 +17,15 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath(){
         foreach(Waypoint waypoint in path){
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+            // LERP (Linear Interpolation).
+            while(travelPercent < 1f){
+                travelPercent += Time.deltaTime;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
