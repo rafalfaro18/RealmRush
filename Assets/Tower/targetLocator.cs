2b4b08d9e7917459ccc28deb3e08b8cacc8393c0
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-28 17:04:55 -0600

    Tower targets closest enemy

--- Assets/Tower/targetLocator.cs
+++ Assets/Tower/targetLocator.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class targetLocator : MonoBehaviour
+public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
     Transform target;
-    
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget(){
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies){
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if(targetDistance < maxDistance){
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        
+        target = closestTarget;
     }
 
     void AimWeapon(){
