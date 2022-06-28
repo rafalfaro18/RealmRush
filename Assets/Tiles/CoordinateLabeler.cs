680e1ea3a3b71abb47a0bf8ff54896952ffe7354
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-28 13:39:36 -0600

    start tile lables disabled

--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -14,6 +14,7 @@ public class CoordinateLabeler : MonoBehaviour
 
     void Awake() {
         label = GetComponent<TextMeshPro>();
+        label.enabled = false;
         waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
