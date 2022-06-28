6b7450eca9e049cf2c67b60e6bbc9f8e4f2c730b
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-28 17:53:26 -0600

    Implements tower projectile range

--- Assets/Tower/targetLocator.cs
+++ Assets/Tower/targetLocator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem projectileParticles;
     Transform target;
 
     void Update()
@@ -26,11 +28,23 @@ public class TargetLocator : MonoBehaviour
                 maxDistance = targetDistance;
             }
         }
-        
+
         target = closestTarget;
     }
 
     void AimWeapon(){
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         weapon.LookAt(target);
+        
+        if(targetDistance < range){
+            Attack(true);
+        } else {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive){
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
