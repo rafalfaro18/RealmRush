bcaabbeb0c21c71ee2c9e98b0cb3cc50a5cbe277
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-29 22:04:55 -0600

    Fix blocking tiles when no money

--- Assets/Environment/Tile.cs
+++ Assets/Environment/Tile.cs
@@ -33,9 +33,11 @@ public class Tile : MonoBehaviour
     void OnMouseDown()
     {
         if(gridManager.GetNode(coordinates).isWalkable && !pathfinder.WillBlockPath(coordinates)){
-            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
-            isPlaceable = !isPlaced;
-            gridManager.BlockNode(coordinates);
+            bool isSuccessful = towerPrefab.CreateTower(towerPrefab, transform.position);
+            
+            if(isSuccessful){
+                gridManager.BlockNode(coordinates);
+            }
         }
     }
 
