9f871d3bfc0e586dc0328e24de38ec94c2d17b2b
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-29 20:31:28 -0600

    Draw the found path

--- Assets/Environment/Tile.cs
+++ Assets/Environment/Tile.cs
@@ -9,6 +9,23 @@ public class Tile : MonoBehaviour
     [SerializeField] bool isPlaceable;
     public bool IsPlaceable { get { return isPlaceable; } }
     
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake(){
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    void Start(){
+        if (gridManager != null) {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if(!isPlaceable){
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+    
     void OnMouseDown()
     {
         if(isPlaceable){
--- README.md
+++ README.md
@@ -4,6 +4,3 @@ Unity demo based on course https://www.udemy.com/course/unitycourse2 Section 5
 ## Controls
 - Left Click = Place a Tower
 - C = Toggle tiles coordinates
-
-## Notes
-- TODO: Move CoordinateLabeler script to the Editor folder before building the project.
