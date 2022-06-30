f05731b70c4f3fca6fee853ab960d4bbab2f23e9
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-06-29 19:53:00 -0600

    Rename Waypoint to Tile

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))] //auto imports Enemy component when importing EnemyHealth comp.
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] List<Tile> path = new List<Tile>();
     [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     Enemy enemy;
@@ -26,7 +26,7 @@ public class EnemyMover : MonoBehaviour
         GameObject parent = GameObject.FindGameObjectWithTag("Path");
 
         foreach(Transform child in parent.transform) {
-            Waypoint waypoint = child.GetComponent<Waypoint>();
+            Tile waypoint = child.GetComponent<Tile>();
             
             if(waypoint != null){
                 path.Add(waypoint);
@@ -46,7 +46,7 @@ public class EnemyMover : MonoBehaviour
 
     IEnumerator FollowPath()
     {
-        foreach (Waypoint waypoint in path)
+        foreach (Tile waypoint in path)
         {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
